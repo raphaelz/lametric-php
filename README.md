b417e6f2cce856fc901c96d2c0741acd7af2b911
raphaelz <raphael@aquaspark.net>
2017-01-07 23:08:39 -0500

    Add frames support and pull application methods

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LaMetric PHP
 
-A PHP wrapper for accessing the LaMetric API. This is a simple example to show how to push data to an Indicator App.
+A PHP wrapper for creating push and pull applications with the LaMetric API.
 
 ## Requirements
 
@@ -20,7 +20,7 @@ To use the LaMetric API you must create a developer account and associate your d
 $ composer require raphaelz/lametric-php
 ```
 
-### Initialize the class
+### Initialize the class with configuration (push application)
 
 ```php
 use Lametric\Lametric;
@@ -31,39 +31,90 @@ $lametric = new Lametric(array(
 ));
 
 ```
-## Available methods
 
-### Send Push Notification
+### Initialize the class without configuration (poll application)
 
 ```php
-$lametric->push("Hello World!");
+use Lametric\Lametric;
+
+$lametric = new Lametric();
+
 ```
 
+## Available methods
+
 ### Change Icon
+
+Set the default icon before pushing a message. This only applies to pushes without an icon # set.
 
 ```php
 // Setting icon number to 95 will show a lightning icon
 $lametric->setIcon(95);
 ```
 
-**The icon number can be found in the icon gallery.**
-**You can also create your own icon, but create a note of the number when it is inserted in the gallery.
+*The icon number can be found in the icon gallery.*
 
+*You can also create your own icon, but make a note of the number when it is inserted in the public gallery.*
 
+### Send Push Notification
+
+The push method allows you to send push notification to Lametric using a simple message, a message with the icon # specified, and an array (frames).
+
+```php
+// Send a simple push notification. Icon must be set using the setIcon method, otherwise it will default to null.
+$lametric->push("Hello World!");
+
+// Send a push notification with specific icon number.
+$lametric->push("Hello World!", 95);
+
+// Send a push notification with an array (frames) as parameter. Must use key, value pairs: text, icon.
+// Note: The frames are not stored after the notification as been sent.
+$lametric->push(array(array('text'=>'Frame 1', 'icon'=>51), array('text'=>'Frame 2', 'icon'=>62)));
+
+```
+
+### Add Frames
+
+With the addFrame and addFrames method, frames can be added for either a push or poll application.
+
+```php
+// Add frame to notification.
+$lametric->addFrame('Notification', 98);
+
+// Add frames for use later
+$lametric->addFrames(array(array('text'=>'Frame 1', 'icon'=>51), array('text'=>'Frame 2', 'icon'=>62)));
+```
+
+### Generate Data
+
+When creating a poll application, use the generateData method in conjunction with the addFrame or addFrames methods to generate the necessary JSON data for the Lametric to retrieve.
+
+```php
+// Return frames as an array
+$lametric->generateData();
+
+// Return frame array as JSON data (poll method)
+$lametric->generateData(true);
+```
+
+### Clear Frames
+```php
+// Clear frames array for object
+$lametric->clearFrames();
+```
 
 ## TODO
 This is an early release of the wrapper, which is in need of some additional features:
 
-- `Support multiple frames`
 - `Support other types of Indicator apps (e.g. Metric, Goal, Sparkline)`
 
 ## Credits
 
-Copyright (c) 2015 - Programmed by Raphael Z.
+Copyright (c) 2017 - Programmed by Raphael Z.
 
 ## License
 
-Copyright (c) 2015 Raphael Z.
+Copyright (c) 2017 Raphael Z.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/Lametric/Lametric.php
+++ src/Lametric/Lametric.php
@@ -23,7 +23,7 @@ class Lametric
      *
      * @var mixed
      */
-	private $_icon = 'i1759';
+	private $_icon = null;
 
     /**
      * A list of HTTP headers required for authorization
@@ -33,23 +33,31 @@ class Lametric
 	private $_http_headers = array();
 
     /**
+     * Array for storing frames
+     * 
+     * @var array
+     */
+    private $_frames = array();
+
+    /**
     * Constructor method
     * 
     * @param    array  $config   Set pushUrl and token variables
     * @return   void
     */
-	public function __construct($config)
+	public function __construct($config=null)
 	{
+        if(!is_null($config)) {
+            $this->setPushURL($config['pushURL']);
+            $this->setToken($config['token']);
+ 
+            $this->_http_headers = array(
+                'Accept: application/json',
+                'X-Access-Token: '.$this->_token,
+                'Pragma: no-cache',
+            );
 
-		$this->setPushURL($config['pushURL']);
-		$this->setToken($config['token']);
-
-		$this->_http_headers = array(
-			'Accept: application/json',
-			'X-Access-Token: '.$this->_token,
-			'Pragma: no-cache',
-		);
-
+        }
 	}
 
     /**
@@ -58,10 +66,10 @@ class Lametric
     * @param    integer  $code    Integer with icon number from gallery
     * @return   void
     */
-	public function setIcon($code)
-	{
-		$this->_icon = 'i'. $code;
-	}
+    public function setIcon($code)
+    {
+        $this->_icon = 'i'. $code;
+    }
 
     /**
     * Set push URL for API calls
@@ -69,10 +77,10 @@ class Lametric
     * @param    string  $pushUrl    String with pushUrl for Indicator App
     * @return   void
     */
-	public function setPushURL($pushUrl)
-	{
-		$this->_pushUrl = $pushUrl;
-	}
+    public function setPushURL($pushUrl)
+    {
+        $this->_pushUrl = $pushUrl;
+    }
 
     /**
     * Set token API calls
@@ -80,43 +88,125 @@ class Lametric
     * @param    string  $token    String with application token
     * @return   void
     */
-	public function setToken($token)
-	{
-		$this->_token = $token;
-	}
+    public function setToken($token)
+    {
+        $this->_token = $token;
+    }
+
+    /**
+    * Add frame to global frames array
+    * 
+    * @param    string  $text    String with desired push notification
+    * @param    number  $icon    Icon ID used for push notification
+    * @return   void
+    */
+    public function addFrame($text, $icon)
+    {
+        if(empty($text)) {
+            throw new LametricException('Missing data in frame parameters.');
+        }
+
+       array_push($this->_frames, array($text, empty($icon) ? $this->_icon : $icon));
+    }
+
+    /**
+    * Add frames to global frames array
+    * 
+    * @param    mixed  $frames    Array with text and icon pairs
+    * @return   void
+    */
+    public function addFrames($frames)
+    {
+        foreach($frames as $frame) {
+
+            if(empty($frame['text'])) {
+                throw new LametricException('Missing data in frame parameters.');
+            }
+
+            $this->addFrame($frame['text'], empty($frame['icon']) ? $this->_icon : $frame['icon']);
+        }  
+    }
+
+    /**
+    * Generate global frames array
+    * 
+    * @param    boolean  $json    Return data as JSON (true)    
+    * @return   string|array
+    */
+    public function generateData($json=false)
+    {
+        if(empty($this->_frames)) throw new LametricException('There is no data to generate.');
+
+        $frames = array('frames'=>array());
+
+       foreach($this->_frames as $key=>$frame) {
+            array_push($frames['frames'], array('index'=>$key, 'text'=>$frame[0], 'icon'=>empty($frame[1]) ? $this->_icon : 'i'.$frame[1]));
+       }
+
+       return (!$json) ? $frames : json_encode($frames);
+    }
+
+    /**
+    * Clear frames array
+    * 
+    * @return   void
+    */
+    public function clearFrames()
+    {
+        unset($this->_frames);
+        $this->_frames = array();
+    } 
 
     /**
     * Send push notification
     * 
-    * @param    string  $text    String with desired push notification
+    * @param    mixed  $text    String with desired push notification
+    * @param    number  $icon    Icon ID used for push notification
     * @throws   Lametric\Exception           If push notification cannot be sent
     */
-	public function push($text)
-	{
 
-		$data_string = json_encode(array(
-				'frames'=>
-				array(
-					array('index'=>0,
-						'text'=>$text,
-						'icon'=>$this->_icon,
-					))));
+    public function push($text = null, $icon = null)
+    {
+        if(empty($this->_pushUrl) || empty($this->_token)) {
+            throw new LametricException('Config needs to be set.');
+        } elseif(empty($this->_frames) && is_null($text)) {
+             throw new LametricException('Empty frames array.');
+        } elseif(is_array($text)) {
+            $this->addFrames($text);
+            $data_string = json_encode($this->generateData());
+            $this->clearFrames();
+        } elseif(is_null($text)) { 
+            $data_string = json_encode($this->generateData());
+        } else {
+            
+            if(empty($this->_frames)) {
+                $data_string = json_encode(array(
+                        'frames'=>
+                        array(
+                            array('index'=>0,
+                                'text'=>$text,
+                                'icon'=>(is_null($icon)) ? $this->_icon : $icon,
+                            ))));
+            } else {
+                $data_string = json_encode(array(
+                        'frames'=>
+                        array(array($this->_frames)))); 
+            }          
+        }
 
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $this->_pushUrl);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->_http_headers);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->_pushUrl);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->_http_headers);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
-		$result = json_decode(curl_exec($ch));
+        $result = json_decode(curl_exec($ch));
 
-		if(isset($result->error) && $result->error) {
-			throw new LametricException($result->error->message . ' - '. $result->error->trace[0]);
-		}
-
-	}
-
+        if(isset($result->error) && $result->error) {
+            throw new LametricException($result->error->message . ' - '. $result->error->trace[0]);
+        }
+    }
 }
